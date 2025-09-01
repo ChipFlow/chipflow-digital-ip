ChipFlow/amaranth-orchard

Clean up public API

chipflow_digital_ip/base/__init__.py
@@ -1,2 +1,4 @@
-from .platform_timer import *
-from .soc_id import *
+from .platform_timer import PlatformTimer
+from .soc_id import SoCID
+
+__all__ = ['PlatformTimer', 'SoCID']

chipflow_digital_ip/io/__init__.py
@@ -1,4 +1,7 @@
-from .gpio import *
-from .uart import *
-from .i2c import *
-from .spi import *
+from .gpio import GPIOPeripheral
+from .uart import UARTPeripheral
+from .i2c import I2CPeripheral, I2CSignature
+from .spi import SPIPeripheral, SPISignature
+
+__all__ = ['GPIOPeripheral', 'UARTPeripheral', 'I2CPeripheral', 'SPIPeripheral',
+           'I2CSignature', 'SPISignature']

ChipFlow/chipflow-digital-ip

chipflow_digital_ip/base/__init__.py
@@ -1,2 +1,4 @@
-from .platform_timer import *
-from .soc_id import *
+from ._platform_timer import PlatformTimer
+from ._soc_id import SoCID
+
+__all__ = ['PlatformTimer', 'SoCID']

chipflow_digital_ip/io/__init__.py
@@ -1,4 +1,7 @@
-from .gpio import *
-from .uart import *
-from .i2c import *
-from .spi import *
+from ._gpio import GPIOPeripheral
+from ._uart import UARTPeripheral
+from ._i2c import I2CPeripheral, I2CSignature
+from ._spi import SPIPeripheral, SPISignature
+
+__all__ = ['GPIOPeripheral', 'UARTPeripheral', 'I2CPeripheral', 'SPIPeripheral',
+           'I2CSignature', 'SPISignature']