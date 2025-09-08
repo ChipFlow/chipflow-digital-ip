8a6890f047ccfd687778045e3c7741409903e21e
Tochi <tochichukwukaanyanwu@gmail.com>
2025-09-08 13:32:08 +0100

    version of GPTimer without excessive autoformatting (#32)

--- chipflow_digital_ip/base/__init__.py
+++ chipflow_digital_ip/base/__init__.py
@@ -1,4 +1,5 @@
 from ._platform_timer import PlatformTimer
 from ._soc_id import SoCID
+from .gptimer import GPTimer
 
-__all__ = ['PlatformTimer', 'SoCID']
+__all__ = ['PlatformTimer', 'SoCID', 'GPTimer']
