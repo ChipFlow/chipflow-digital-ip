bc423d98d85dac3dcd53d07a81eb924c487b4462
Rob Taylor <rob.taylor@chipflow.io>
2025-09-10 14:21:02 +0100

    wip convert hyperram test to async

--- tests/test_hyperram.py
+++ tests/test_hyperram.py
@@ -2,41 +2,41 @@
 
 # SPDX-License-Identifier: BSD-2-Clause
 
+import warnings
+
 from amaranth import Module
-from amaranth.sim import Simulator
+from amaranth.sim import Simulator, Tick
+from amaranth.hdl import UnusedElaboratable
+
 from chipflow_digital_ip.memory import HyperRAM
 
 
 def test_hyperram_smoke():
+
+    warnings.simplefilter(action="ignore", category=UnusedElaboratable)
+
     m = Module()
     m.submodules.hram = hram = HyperRAM()
     sim = Simulator(m)
     sim.add_clock(1e-6)
 
-    def process():
-        yield hram.data_bus.adr.eq(0x5A5A5A)
-        yield hram.data_bus.dat_w.eq(0xF0F0F0F0)
-        yield hram.data_bus.sel.eq(1)
-        yield hram.data_bus.we.eq(1)
-        yield hram.data_bus.stb.eq(1)
-        yield hram.data_bus.cyc.eq(1)
-        for i in range(100):
-            if (yield hram.data_bus.ack):
-                yield hram.data_bus.stb.eq(0)
-                yield hram.data_bus.cyc.eq(0)
-            yield
-        yield hram.data_bus.adr.eq(0x5A5A5A)
-        yield hram.data_bus.sel.eq(1)
-        yield hram.data_bus.we.eq(0)
-        yield hram.data_bus.stb.eq(1)
-        yield hram.data_bus.cyc.eq(1)
-        yield hram.pins.rwds.i.eq(1)
-        yield hram.pins.dq.i.eq(0xFF)
-        for i in range(100):
-            if (yield hram.data_bus.ack):
-                yield hram.data_bus.stb.eq(0)
-                yield hram.data_bus.cyc.eq(0)
-            yield
-    sim.add_sync_process(process)
+    async def process():
+        hram.data_bus.adr.eq(0x5A5A5A)
+        hram.data_bus.dat_w.eq(0xF0F0F0F0)
+        hram.data_bus.sel.eq(1)
+        hram.data_bus.we.eq(1)
+        hram.data_bus.stb.eq(1)
+        hram.data_bus.cyc.eq(1)
+        await sim.tick().sample(hram.data_bus.ack and  hram.data_bus.stb and hram.data_bus.cyc).until(1)
+
+        hram.data_bus.adr.eq(0x5A5A5A)
+        hram.data_bus.sel.eq(1)
+        hram.data_bus.we.eq(0)
+        hram.data_bus.stb.eq(1)
+        hram.data_bus.cyc.eq(1)
+        hram.pins.rwds.i.eq(1)
+        hram.pins.dq.i.eq(0xFF)
+        await sim.tick().sample(hram.data_bus.ack and  hram.data_bus.stb and hram.data_bus.cyc).until(1)
+    sim.add_testbench(process)
     with sim.write_vcd("hyperram.vcd", "hyperram.gtkw"):
         sim.run()
