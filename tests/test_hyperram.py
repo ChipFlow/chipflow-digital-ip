75790869238e2b5fdfa793e8879b4c9c45c9019c
Rob Taylor <rob.taylor@chipflow.io>
2025-11-13 09:28:05 +0000

    Update hyperram test to async api

--- tests/test_hyperram.py
+++ tests/test_hyperram.py
@@ -19,30 +19,30 @@ def test_hyperram_smoke():
     sim = Simulator(m)
     sim.add_clock(1e-6)
 
-    def process():
-        yield hram.data_bus.adr.eq(0x5A5A5A)
-        yield hram.data_bus.dat_w.eq(0xF0F0F0F0)
-        yield hram.data_bus.sel.eq(1)
-        yield hram.data_bus.we.eq(1)
-        yield hram.data_bus.stb.eq(1)
-        yield hram.data_bus.cyc.eq(1)
+    async def process(ctx):
+        ctx.set(hram.data_bus.adr, 0x5A5A5A)
+        ctx.set(hram.data_bus.dat_w, 0xF0F0F0F0)
+        ctx.set(hram.data_bus.sel, 1)
+        ctx.set(hram.data_bus.we, 1)
+        ctx.set(hram.data_bus.stb, 1)
+        ctx.set(hram.data_bus.cyc, 1)
         for i in range(100):
-            if (yield hram.data_bus.ack):
-                yield hram.data_bus.stb.eq(0)
-                yield hram.data_bus.cyc.eq(0)
-            yield
-        yield hram.data_bus.adr.eq(0x5A5A5A)
-        yield hram.data_bus.sel.eq(1)
-        yield hram.data_bus.we.eq(0)
-        yield hram.data_bus.stb.eq(1)
-        yield hram.data_bus.cyc.eq(1)
-        yield hram.pins.rwds.i.eq(1)
-        yield hram.pins.dq.i.eq(0xFF)
+            if ctx.get(hram.data_bus.ack):
+                ctx.set(hram.data_bus.stb, 0)
+                ctx.set(hram.data_bus.cyc, 0)
+            await ctx.tick()
+        ctx.set(hram.data_bus.adr, 0x5A5A5A)
+        ctx.set(hram.data_bus.sel, 1)
+        ctx.set(hram.data_bus.we, 0)
+        ctx.set(hram.data_bus.stb, 1)
+        ctx.set(hram.data_bus.cyc, 1)
+        ctx.set(hram.pins.rwds.i, 1)
+        ctx.set(hram.pins.dq.i, 0xFF)
         for i in range(100):
-            if (yield hram.data_bus.ack):
-                yield hram.data_bus.stb.eq(0)
-                yield hram.data_bus.cyc.eq(0)
-            yield
-    sim.add_sync_process(process)
+            if ctx.get(hram.data_bus.ack):
+                ctx.set(hram.data_bus.stb, 0)
+                ctx.set(hram.data_bus.cyc, 0)
+            await ctx.tick()
+    sim.add_testbench(process)
     with sim.write_vcd("hyperram.vcd", "hyperram.gtkw"):
         sim.run()
